605ec68b04898b1b11394af1ff1e32c1d4b6159d
linzh17 <15018279123@163.com>
2018-06-02 17:47:21 +0800

    edit2

--- petdata/models.py
+++ petdata/models.py
@@ -9,9 +9,29 @@ class User(models.Model):
     email = models.CharField("email",max_length=128,unique = True,default = "0")
     password = models.IntegerField(default=1) 
 
-    #friends = models.ForeignKey(User)
+    #friends = models.ForeignKey('self',default)
 
     def __str__(self):
         return 'User %s %s' %(self.user_name,self.email)
 
         
+class Pet(models.Model):
+    pet_name = models.CharField(max_length=128,primary_key=True)
+    owner = models.ForeignKey(User)
+    def __str__(self):
+        return 'Pet %s' %(self.pet_name)
+
+
+class Pet_State(models.Model):
+    Pet_name= models.OneToOneField(Pet,primary_key=True, on_delete=models.CASCADE)
+    pet_hunger = models.IntegerField(default=0)
+    pet_clean = models.IntegerField(default=0)
+    pet_love = models.IntegerField(default=0)
+
+    def __str__(self):
+        return 'Pet_State %s %s %s' %(self.pet_hunger,self.pet_clean,self.pet_love)
+
+        
+
+        
+
