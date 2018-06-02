fc1993136b6f23cce8b26245d19f70a76356e9d7
Chenduoru <chenduoru21@qq.com>
2018-06-02 20:34:52 +0800

    day

--- petdata/views.py
+++ petdata/views.py
@@ -4,15 +4,40 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+import models
+
+import json
 # Create your views here.
 
-def func1(request):
-	return HttpResponse("1")
+def signUp(request):
+	username = request.GET['username']
+	password = request.GET['password']
+	Attr = models.User(user_name = username , password = password)
+	Attr.save()
+	Attrjson = {'username': Attr.user_name}
+	return HttpResponse(json.dumps(Attrjson))
 
-def func2(request):
-	return HttpResponse("2")
 
-def func3(request):
+def login(request):
+	print request
+	username = request.GET['username']
+	password = request.GET['password']
+	Attr = models.User.objects.get(user_name = username , password = password)
+	Attrjson = {'username': Attr.user_name}
+	return HttpResponse(json.dumps(Attrjson))
+		
+
+def createPet(request):
+	owner = request.GET['username']
+	petname = request.GET['petname']
+	pet = models.Pet(pet_name = petname, owner = owner )
+	pet.save()
+	petstatus = model.Pet_State(pet_name = pet)
+	return HttpResponse("success")
+
+def petStatus(request):
+	petname = request.GET['']
 	return HttpResponse("3")
 
 def func4(request):
